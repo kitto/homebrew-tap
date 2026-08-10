65722b0c91d7985856f88faae22c808287c4527e
Matt Pilott <matt@pilott.me>
2026-08-10 17:45:48 +0100

    kitto-zsh 0.7.0

--- Formula/kitto-zsh.rb
+++ Formula/kitto-zsh.rb
@@ -1,23 +1,25 @@
 class KittoZsh < Formula
   desc "Backpack of zsh helpers for Mac JS development"
   homepage "https://github.com/mattpilott/kitto-zsh"
-  url "https://github.com/mattpilott/kitto-zsh/archive/refs/tags/v0.6.2.tar.gz"
-  sha256 "4b04cc8db3693d41e0856e4db214558b9545a46b665fe9e952d7a189a63851c1"
+  url "https://github.com/mattpilott/kitto-zsh/archive/refs/tags/v0.7.0.tar.gz"
+  sha256 "93e399f1cc68e5bcac13a36ff516015b04428478f2c38e124f1cc33b262333d9"
   license "MIT"
 
   def install
     prefix.install "kitto-zsh.plugin.zsh", "functions"
+    bin.install "bin/kitto-zsh"
     zsh_completion.install "completions/_kitto-zsh"
   end
 
   def caveats
     <<~EOS
-      Add to ~/.zshrc:
-        source #{opt_prefix}/kitto-zsh.plugin.zsh
+      Run this once to wire it into ~/.zshrc:
+        kitto-zsh init
     EOS
   end
 
   test do
     system "zsh", "-n", "#{opt_prefix}/kitto-zsh.plugin.zsh"
+    assert_match "usage", shell_output("#{bin}/kitto-zsh 2>&1", 1)
   end
 end
